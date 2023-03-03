07055ccbf39c92b0189f039907e9a74ccb2af2b8
José Sánchez-Gallego <gallegoj@uw.edu>
2023-03-03 19:05:16 -0300

    Allow to select configuration file

--- src/lvm_spec_pressure/__main__.py
+++ src/lvm_spec_pressure/__main__.py
@@ -18,21 +18,27 @@ from lvm_spec_pressure.server import SpecPressureServer
 
 @click.command()
 @click.argument("SPEC", type=str)
+@click.option("-c", "--config", type=str, help="Path to configuration file.")
 @click.option("--debug", is_flag=True, help="Runs in debug mode.")
 @cli_coro()
-async def lvm_spec_pressure(spec: str, debug: bool = False):
+async def lvm_spec_pressure(spec: str, config: str | None = None, debug: bool = False):
     """Start a TCP-to-COM server."""
 
     if spec is None:
         raise click.MissingParameter("spec argument must be provided.")
 
-    cwd = pathlib.Path(__file__).parent
-    config = read_yaml_file(cwd / "etc/lvm_spec_pressure.yaml")
+    if config is not None:
+        config_data = read_yaml_file(config)
+    else:
+        config = str(pathlib.Path(__file__).parent / "etc/lvm_spec_pressure.yaml")
 
-    if spec not in config["specs"]:
+    print("Config file", config)
+    config_data = read_yaml_file(config)
+
+    if spec not in config_data["specs"]:
         raise ValueError(f"spec {spec} not found in configuration.")
 
-    cameras = list(config["specs"][spec].keys())
+    cameras = list(config_data["specs"][spec].keys())
 
     servers = await asyncio.gather(
         *[SpecPressureServer(spec, camera).start() for camera in cameras]
